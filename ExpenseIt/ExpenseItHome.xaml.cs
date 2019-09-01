a313ae19de9e70e7e6d929977b9dfe7006b91de5
Jason Williams <jasonwilliams2308@gmail.com>
2019-09-01 14:54:10 +0100

    Created base project and two pages, image and grid.

---
+++ ExpenseIt/ExpenseItHome.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows.Controls;
+
+/*
+    When you create a new Page file, Visual Studio automatically creates its code-behind file. These code-behind files handle the logic for responding to user input.
+ */
+
+namespace ExpenseIt
+{
+    /// <summary>
+    /// Interaction logic for ExpenseItHome.xaml
+    /// </summary>
+    public partial class ExpenseItHome : Page
+    {
+        public ExpenseItHome()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- ExpenseIt/MainWindow.xaml.cs
+++ ExpenseIt/MainWindow.xaml.cs
@@ -1,24 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows.Navigation;
+
+/* This file is a code-behind file that contains code to handle the events declared in MainWindow.xaml. This file contains a partial class for the window defined in XAML.*/
 
 namespace ExpenseIt
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    ///
+
+    public partial class MainWindow : NavigationWindow
     {
         public MainWindow()
         {
